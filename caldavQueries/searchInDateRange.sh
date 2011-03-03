2c0bdbe795ef901c4a56adaf6eb990bcd7d710a0
Chris Tweney <chris@media.berkeley.edu>
2011-03-03 13:52:34 -0800

    Pretty up the XML

--- caldavQueries/searchInDateRange.sh
+++ caldavQueries/searchInDateRange.sh
@@ -3,26 +3,29 @@
 STARTDATE="20110305T000000Z"
 ENDDATE="20110315T000000Z"
 SERVER_URL="http://test.media.berkeley.edu:8080/ucaldav/principals/users/vbede/"
+USER_PASS="vbede:bedework"
 
 XML="
 <?xml version='1.0'?>
-	<c:calendar-query xmlns:c='urn:ietf:params:xml:ns:caldav'>
-        <d:prop xmlns:d='DAV:'>
+<c:calendar-query xmlns:c='urn:ietf:params:xml:ns:caldav'>
+    <d:prop xmlns:d='DAV:'>
         <d:getetag/>
         <c:calendar-data>
         </c:calendar-data>
-        </d:prop>
-        <c:filter>
-            <c:comp-filter name='VCALENDAR'>
-          <c:comp-filter name='VEVENT'>
-            <c:time-range start='$STARTDATE' end='$ENDDATE'/>
-          </c:comp-filter>
+    </d:prop>
+    <c:filter>
+        <c:comp-filter name='VCALENDAR'>
+            <c:comp-filter name='VEVENT'>
+                <c:time-range start='$STARTDATE' end='$ENDDATE'/>
+            </c:comp-filter>
         </c:comp-filter>
-      </c:filter>
-    </c:calendar-query>
+    </c:filter>
+</c:calendar-query>
 "
 
+# add \ before newlines
 XML=`echo "${XML}" | tr '\n' '\\\n'`
 
-curl -i -u vbede:bedework -X REPORT $SERVER_URL -d "${XML}"
+curl -i -u $USER_PASS -X REPORT $SERVER_URL -d "${XML}"
 
+echo
