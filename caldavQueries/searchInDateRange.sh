95b9b6b24728de2f46548a3693f73d8030592ba8
Chris Tweney <chris@media.berkeley.edu>
2011-03-03 13:24:15 -0800

    Bash script to run caldav queries without having to escape every line of XML

---
+++ caldavQueries/searchInDateRange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+STARTDATE="20110305T000000Z"
+ENDDATE="20110315T000000Z"
+SERVER_URL="http://test.media.berkeley.edu:8080/ucaldav/principals/users/vbede/"
+
+XML="
+<?xml version='1.0'?>
+	<c:calendar-query xmlns:c='urn:ietf:params:xml:ns:caldav'>
+        <d:prop xmlns:d='DAV:'>
+        <d:getetag/>
+        <c:calendar-data>
+        </c:calendar-data>
+        </d:prop>
+        <c:filter>
+            <c:comp-filter name='VCALENDAR'>
+          <c:comp-filter name='VEVENT'>
+            <c:time-range start='$STARTDATE' end='$ENDDATE'/>
+          </c:comp-filter>
+        </c:comp-filter>
+      </c:filter>
+    </c:calendar-query>
+"
+
+XML=`echo "${XML}" | tr '\n' '\\\n'`
+
+curl -i -u vbede:bedework -X REPORT $SERVER_URL -d "${XML}"
+
