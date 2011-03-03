330a23decc2da7253da4c7f1d70ca187aef1ee19
Bedework User <bedework@test.Media.Berkeley.EDU>
2011-03-03 11:25:37 -0800

    Updated build/start scripts to use new shadow customization directory system

--- buildBedework.sh
+++ buildBedework.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd /home/bedework/bedework/quickstart-3.7
-./bw -bwc default deploy.debug
+BEDE_HOME=~/bedework/quickstart-3.7
+
+cd $BEDE_HOME
+cp -R ~/quickstart_customizations/* .
+./bw -bwchome $BEDE_HOME/bedework/config/bwbuild -bwc default deploy.debug
 
 
--- startBedework.sh
+++ startBedework.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd ~/bedework/quickstart-3.7
-nohup ./bw -bwc default dirstart > logs/apacheds.log 2>&1 &
+BEDE_HOME=~/bedework/quickstart-3.7
+
+cd $BEDE_HOME
+nohup ./bw -bwchome $BEDE_HOME/bedework/config/bwbuild -bwc default dirstart > logs/apacheds.log 2>&1 &
 nohup ./startjboss > logs/jboss.log 2>&1 &
 
-echo "Server started, check logs in ~/bedework/quickstart-3.7/logs"
+echo "Server started, check logs in $BEDE_HOME/logs"
 
 
