b3cf45672bd98550dc02c50eb818102bb382032c
Chris Tweney <chris@media.berkeley.edu>
2011-03-03 13:26:58 -0800

    Build script now updates from git before building

--- buildBedework.sh
+++ buildBedework.sh
@@ -2,6 +2,9 @@
 
 BEDE_HOME=~/bedework/quickstart-3.7
 
+cd ~-
+git pull
+
 cd $BEDE_HOME
 cp -R ~/quickstart_customizations/* .
 ./bw -bwchome $BEDE_HOME/bedework/config/bwbuild -bwc default deploy.debug
