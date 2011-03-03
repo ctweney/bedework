a1806f26acbb6568bd7b7b9507a745c021c18b08
Chris Tweney <chris@media.berkeley.edu>
2011-03-03 14:32:38 -0800

    More caldav query knowhow

--- caldavQueries/searchInDateRange.sh
+++ caldavQueries/searchInDateRange.sh
@@ -5,27 +5,53 @@ ENDDATE="20110315T000000Z"
 SERVER_URL="http://test.media.berkeley.edu:8080/ucaldav/principals/users/vbede/"
 USER_PASS="vbede:bedework"
 
-XML="
-<?xml version='1.0'?>
-<c:calendar-query xmlns:c='urn:ietf:params:xml:ns:caldav'>
-    <d:prop xmlns:d='DAV:'>
-        <d:getetag/>
-        <c:calendar-data>
-        </c:calendar-data>
-    </d:prop>
-    <c:filter>
-        <c:comp-filter name='VCALENDAR'>
-            <c:comp-filter name='VEVENT'>
-                <c:time-range start='$STARTDATE' end='$ENDDATE'/>
-            </c:comp-filter>
-        </c:comp-filter>
-    </c:filter>
-</c:calendar-query>
-"
+XML='<?xml version="1.0" encoding="utf-8" ?>
+<C:calendar-query xmlns:D="DAV:"
+              xmlns:C="urn:ietf:params:xml:ns:caldav">
+  <D:prop>
+    <D:getetag/>
+    <C:calendar-data>
+      <C:comp name="VCALENDAR">
+        <C:prop name="VERSION"/>
+        <C:comp name="VEVENT">
+          <C:prop name="SUMMARY"/>
+          <C:prop name="UID"/>
+          <C:prop name="DTSTART"/>
+          <C:prop name="DTEND"/>
+          <C:prop name="DURATION"/>
+          <C:prop name="RRULE"/>
+          <C:prop name="RDATE"/>
+          <C:prop name="EXRULE"/>
+          <C:prop name="EXDATE"/>
+          <C:prop name="RECURRENCE-ID"/>
+        </C:comp>
+        <C:comp name="VTIMEZONE"/>
+      </C:comp>
+    </C:calendar-data>
+  </D:prop>
+  <C:filter>
+    <C:comp-filter name="VCALENDAR">
+      <C:comp-filter name="VEVENT">
+        <C:time-range start="$STARTDATE"
+                      end="$ENDDATE"/>
+      </C:comp-filter>
+    </C:comp-filter>
+  </C:filter>
+</C:calendar-query>
+'
 
-# add \ before newlines
-XML=`echo "${XML}" | tr '\n' '\\\n'`
+# strip newlines
+XML=`echo "${XML}" | tr -d '\n'`
 
-curl -i -u $USER_PASS -X REPORT $SERVER_URL -d "${XML}"
+#echo "Getting OPTIONS"
+#curl -i -u $USER_PASS -X OPTIONS $SERVER_URL
+#echo
 
+echo "Doing PROPFIND"
+curl -v -i -u $USER_PASS -X PROPFIND $SERVER_URL -H "Depth: infinity"
 echo
+
+
+#curl -v -i -u $USER_PASS -X REPORT $SERVER_URL -d "$XML"
+
+
