91a2e5ffd7346f2e4af27ca8a5d03cc7b287d8f9
Chris Tweney <chris@media.berkeley.edu>
2011-03-09 09:08:44 -0800

    Start JBoss in debug mode

--- startBedework.sh
+++ startBedework.sh
@@ -4,7 +4,7 @@ BEDE_HOME=~/bedework/quickstart-3.7
 
 cd $BEDE_HOME
 nohup ./bw -bwchome $BEDE_HOME/bedework/config/bwbuild -bwc default dirstart > logs/apacheds.log 2>&1 &
-nohup ./startjboss > logs/jboss.log 2>&1 &
+nohup ./startjboss -debug > logs/jboss.log 2>&1 &
 
 echo "Server started, check logs in $BEDE_HOME/logs"
 
